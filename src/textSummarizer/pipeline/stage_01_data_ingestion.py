e3ddd14f6b1991b937e71c1cba62e7bdf57ec0bc
Vishwajit <vishwajitsarakpatil@email.com>
2025-03-24 02:22:00 +0530

    DATA_INGESTION

---
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,18 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_ingestion import DataIngestion
+from textSummarizer.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        logger.info(f"Data Ingestion Config: {data_ingestion_config.__dict__}")
+
+    # Example of using DataIngestion (assuming DataIngestion class is defined elsewhere)
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
--- template.py
+++ template.py
@@ -45,3 +45,4 @@ for filepath in list_of_files:
 
     else:
         logging.info(f"{filename} is already exists")
+        
